7e83a8dc8547f819a245135baec05a817e849386
Emmanuel-Obiora <emmanuelobiora94@gmail.com>
2023-01-18 14:00:09 +0100

    Adding responsiveness to header landmark

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,39 @@ import React from 'react'
 import './Header.css'
 import { Link } from "react-router-dom";
 import { FcHome } from 'react-icons/fc'
+import { AiOutlineClose } from 'react-icons/ai'
 
 
 const Header = () => {
+
+    const showNav = () => {
+        const display = document.getElementById('Navigation');
+        const modal = document.getElementById('Modal');
+        const close = document.getElementById('Closex');
+        const hide = document.getElementById('Toggleb');
+
+        display.classList.add('show-nav');
+        modal.classList.add('open-modal');
+        close.classList.add('open-modal');
+        hide.classList.add('seeing');
+    };
+
+    const hideNav = () => {
+        const display = document.getElementById('Navigation');
+        const modal = document.getElementById('Modal');
+        const close = document.getElementById('Closex');
+        const hide = document.getElementById('Toggleb');
+
+        display.classList.remove('show-nav');
+        modal.classList.remove('open-modal');
+        close.classList.remove('open-modal');
+        hide.classList.remove('seeing');
+    };
+
     return (
         <header>
             <Link to='/'><FcHome className="home" /></Link>
-            <nav>
+            <nav id="Navigation">
                 <ul>
                     <li><Link to="/newbie" className="nav-list">Newbie</Link></li>
                     <li><Link to="/junior" className="nav-list">Junior</Link></li>
@@ -17,6 +43,11 @@ const Header = () => {
                     <li><Link to="/guru" className="nav-list">Guru</Link></li>
                 </ul>
             </nav>
+            <div className="toggle" id="Toggleb" onClick={showNav}>
+                <span className="menu-bar"></span>
+            </div>
+            <AiOutlineClose className="close-btn" id="Closex" onClick={hideNav} />
+            <span className="modal" id='Modal' onClick={hideNav}></span>
         </header>
     )
 }
